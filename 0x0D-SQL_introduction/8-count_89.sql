bbe2b99549ac5574b83f9c897b560e7086e91d26
User <me@example.com>
2024-06-12 16:19:03 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- A script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+SELECT COUNT(*) FROM first_table WHERE id = 89;
